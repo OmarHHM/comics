152f13010a74a133eaaacc3262a0cd49b2a7bdd5
Omar <ing_omarhdez@hotmail.com>
2021-01-24 16:34:43 -0600

    Modificación en archivo sh para iniciar los proyectos.

--- assemble.sh
+++ assemble.sh
@@ -5,13 +5,13 @@ cd alboComicsScheduler
 echo 'Starting Job...'
 
 mvn clean install
-mvn spring-boot:run &
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Djdk.tls.client.protocols=TLSv1.2" &
 
 echo 'Job started...' 
 echo -e "\n"
 echo -e "\n"
 echo -e "\n"
-cd ..
+
 cd ..
 
 cd alboComics
@@ -19,10 +19,9 @@ cd alboComics
 echo 'Starting API...'
 
 mvn clean install
-mvn spring-boot:run &
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Djdk.tls.client.protocols=TLSv1.2" &
 
 echo 'API Started...'
 echo -e "\n"
 echo -e "\n"
 echo -e "\n"
-exit 0
